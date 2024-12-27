5f7262451adb373b6c391620dda1dbd3a85c2c1b
GitUserMichael <Saveliev.mix@mail.ru>
2024-12-27 20:04:50 +0300

    Initial commit 1 station

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+class Point
+{
+private:
+    double m_a;
+    double m_b;
+public:
+    Point(double m_a = 0, double m_b = 0)
+    {
+        this->m_a = m_a;
+        this->m_b = m_b;
+    }
+    void print()
+    {
+        std::cout << "Point(" << m_a << ", " << m_b << ")" << std::endl;
+    }
+};
+int main()
+{
+    Point first;
+    Point second(2.0, 5.0);
+    first.print();
+    second.print();
+
+    return 0;
+}
